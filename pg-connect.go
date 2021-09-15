d31169c54754f74a6631147b037ed35e7c958906
Your Name <windingroad100hf@gmail.com>
2021-09-14 23:31:21 -0400

    complete postgres database with search functionality and routing

--- pg-connect.go
+++ pg-connect.go
@@ -2,140 +2,123 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
-    // "net/http"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
+
+	// "net/http"
 	_ "github.com/lib/pq"
 )
 
 const (
 	// TODO fill this in directly or through environment variable
-    host = "localhost" 
-    port = 5432
-    user = "postgres"
-    dbname = "myblog"
-    password = "Trombone88!"
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	dbname   = "myblog"
+	password = "Trombone88!"
 )
 
-type sandbox struct {
-    post_id int
-    tlte string 
-    content string
-    author string
-    description string
-    publication_date string
+type Blogpost struct {
+	Post_id          string
+	Title            string
+	Content          string
+	Author           string
+	Description      string
+	Publication_date string
 }
-    var psqlconn = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
 
-    var db, err = sql.Open("postgres", psqlconn)
-func main(){
-    db_connect()
-    getEntry()
-    // http.HandleFunc("/retrieve", retrieveRecord) // (1)
-    // http.ListenAndServe(":8080", nil) // (2)
-    
+var Blogposts []Blogpost
+var psqlconn = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+var db, err = sql.Open("postgres", psqlconn)
+
+func main() {
+	db_connect()
+	getAll()
+	handleRequests()
+
 }
 func db_connect() {
-    // connection string
-    // psqlconn := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
-    
-        // open database
-    // db, err := sql.Open("postgres", psqlconn)
-    CheckError(err)
-        
-        // close database
-    
-        // check db
-    err = db.Ping()
-    CheckError(err)
-    
-    fmt.Println("Connected!")
-}
-     
+	CheckError(err)
 
+	// close database
 
-func getEntry(){
-    rowsRs, err := db.Query(`SELECT * FROM "posts"`)
-    defer rowsRs.Close()
-    for rowsRs.Next() {
-        var post_id int
-        var title string 
-        var content string
-        var author string
-        var description string
-        var publication_date string
-        err = rowsRs.Scan(&post_id, &title, &content, &author, &description, &publication_date)
-        CheckError(err)
-        fmt.Println(title, author, content, author, description)
-    }
+	// check db
+	err = db.Ping()
+	CheckError(err)
 
+	fmt.Println("Connected!")
 }
 
+func getAll() {
+	// returnEntry := []string{"a", "a", "a", "a", "a", "a"}
+	rowsRs, err := db.Query(`SELECT * FROM "posts"`)
+	defer rowsRs.Close()
+	for rowsRs.Next() {
+		var post_id string
+		var title string
+		var content string
+		var author string
+		var description string
+		var publication_date string
+		err = rowsRs.Scan(&post_id, &title, &content, &author, &description, &publication_date)
+		CheckError(err)
+		blogpost := Blogpost{post_id, title, content, author, description, "testdate"}
+		Blogposts = append(Blogposts, blogpost)
+		fmt.Println(err)
+		fmt.Println(Blogposts)
+	}
+}
 
 func CheckError(err error) {
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 }
 
-// func retrieveRecord(w http.ResponseWriter, r *http.Request) {
+func homePage(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 
-//     fmt.Println("shnoodle")
-//     // checks if the request is a "GET" request
-//     if r.Method != "GET" {
-//         fmt.Println("not get")
-//         http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
-//     return
-//     }
+}
 
-//     // We assign the result to 'rows'
-//     fmt.Println(db.Query("SELECT * FROM posts"))
+func returnAllPosts(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Blogposts)
+}
 
-//     rowsRs, err := db.Query("SELECT * FROM posts")
-//     if err != nil {
-//         fmt.Println("nono")
-//         fmt.Println(err)
-//         http.Error(w, http.StatusText(500), http.StatusInternalServerError)
-//     return
-//     }
-//     defer rowsRs.Close()
-//     for rowsRs.Next() {
-//         // var post_id int
-//         var title string 
-//         // var content string
-//         var author string
-//         // var description string
-//         // var publication_date string
+func returnSinglePost(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	vars := mux.Vars(r)
+	dashTitle := strings.Replace(vars["title"], " ", "-", -1)
 
+	// Loop over all of our Articles
+	// if the article.Id equals the key we pass in
+	// return the article encoded as JSON
+	for _, post := range Blogposts {
+		title := strings.Replace(post.Title, " ", "-", -1)
+		if title == dashTitle {
+			json.NewEncoder(w).Encode(post)
+		}
+	}
+}
 
-//         err = rowsRs.Scan(&title, &author)
-//         fmt.Println(title, author)
-//     }
-
-
-    // creates placeholder of the sandbox
-    // snbs := make([]sandbox, 0)
-
-    // we loop through the values of rows
-    // for rowsRs.Next() {
-        // snb := sandbox{}
-
-    //     err := rowsRs.Scan(&snb.post_id, &snb.title, &snb.content, &snb.content &snb.author, &snb.description, &snb.publication_date)
-    //     if err != nil {
-    //     log.Println(err)
-    //     http.Error(w, http.StatusText(500), 500)
-    //     return
-    //     }
-    //     snbs = append(snbs, snb)
-    // }
-
-    // if err = rowsRs.Err(); err != nil {
-    //     http.Error(w, http.StatusText(500), 500)
-    //     return
-    // }
-
-    // // loop and display the result in the browser
-    // for _, snb := range snbs {
-    //     fmt.Fprintf("Hello")
-    // }
-
-// }
+func handleRequests() {
+	// http.HandleFunc("/", homePage)
+	// http.HandleFunc("/posts", returnAllPosts)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/posts", returnAllPosts)
+	myRouter.HandleFunc("/posts/{title}", returnSinglePost)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
