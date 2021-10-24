b2c3e13d6ebf27fe681d998589fccf4a51bbfade
Your Name <windingroad100hf@gmail.com>
2021-10-24 13:25:01 -0400

    feat(added sorting and date formattgin)
    This formats the dates correctly and sorts
    the blog posts in order from newest to oldest.
    
    windingroad100hf

--- pg-connect.go
+++ pg-connect.go
@@ -7,7 +7,8 @@ import (
 	"log"
 	"net/http"
 	"strings"
-
+	"time"
+    "sort"
 	"github.com/gorilla/mux"
 
 	// "net/http"
@@ -29,7 +30,7 @@ type Blogpost struct {
 	Content          string
 	Author           string
 	Description      string
-	Publication_date string
+	Publication_date string 
 }
 
 var Blogposts []Blogpost
@@ -40,6 +41,15 @@ var db, err = sql.Open("postgres", psqlconn)
 func main() {
 	db_connect()
 	getAll()
+	// timex := (Blogposts[0].Publication_date)
+	// t, err := time.Parse(time.RFC3339, timex)
+	// timey := t.Format(time.Stamp)
+	// fmt.Println("UNIX: ", timey)
+	if err != nil {
+		log.Fatal(err)
+	}
+	//fmt.Printf("date is of type %T \n", t)
+//	fmt.Printf("date is %v", t)
 	handleRequests()
 
 }
@@ -55,8 +65,21 @@ func db_connect() {
 	fmt.Println("Connected!")
 }
 
+func clearBlogposts() {
+	Blogposts = nil
+	fmt.Println("")
+	fmt.Println("Clearing Blogs")
+	fmt.Println("")
+}
+
+func refreshBlogposts() {
+	clearBlogposts()
+	fmt.Println("Refreshing ")
+	getAll()
+}
+
 func getAll() {
-	// returnEntry := []string{"a", "a", "a", "a", "a", "a"}
+	Blogposts = Blogposts[:0]
 	rowsRs, err := db.Query(`SELECT * FROM "posts"`)
 	defer rowsRs.Close()
 	for rowsRs.Next() {
@@ -65,14 +88,22 @@ func getAll() {
 		var content string
 		var author string
 		var description string
-		var publication_date string
+		var publication_date time.Time 
 		err = rowsRs.Scan(&post_id, &title, &content, &author, &description, &publication_date)
 		CheckError(err)
-		blogpost := Blogpost{post_id, title, content, author, description, "testdate"}
+
+        // formatting the current timestamp in unixDate format
+        publication_date_string := publication_date.Format(time.UnixDate)
+        // generating a string of the unixdate format
+        unix_time, err := time.Parse(time.UnixDate, publication_date_string)
+        // formatting that string to be nice
+        pubdate := unix_time.Format("January 2, 2006") 
+		blogpost := Blogpost{post_id, title, content, author, description, pubdate}
+
 		Blogposts = append(Blogposts, blogpost)
 		fmt.Println(err)
-		fmt.Println(Blogposts)
 	}
+    SortByDate(Blogposts)
 }
 
 func CheckError(err error) {
@@ -81,7 +112,24 @@ func CheckError(err error) {
 	}
 }
 
+func SortByDate(blogs []Blogpost) {
+    sort.Slice(blogs, func(i, j int) bool {
+        t1, err1 := time.Parse("January 2, 2006", blogs[i].Publication_date)
+        t1a := t1.Format(time.RFC3339)
+        t2, err2 := time.Parse("January 2, 2006", blogs[j].Publication_date)
+        t2a := t2.Format(time.RFC3339)
+        if err1  != nil || err2 != nil {
+            fmt.Println("error with SortByDateFunction")
+        }
+        return t1a > t2a 
+
+        })
+
+    fmt.Println("SORTED")
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(Blogposts[0].Publication_date)
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Fprintf(w, "Welcome to the HomePage!")
@@ -90,6 +138,7 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func returnAllPosts(w http.ResponseWriter, r *http.Request) {
+	getAll()
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	fmt.Println("Endpoint Hit: returnAllArticles")
@@ -100,13 +149,13 @@ func returnSinglePost(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	vars := mux.Vars(r)
-	dashTitle := strings.Replace(vars["title"], " ", "-", -1)
+	dashTitle := strings.ToLower(strings.Replace(vars["title"], " ", "-", -1))
 
 	// Loop over all of our Articles
 	// if the article.Id equals the key we pass in
 	// return the article encoded as JSON
 	for _, post := range Blogposts {
-		title := strings.Replace(post.Title, " ", "-", -1)
+		title := strings.ToLower(strings.Replace(post.Title, " ", "-", -1))
 		if title == dashTitle {
 			json.NewEncoder(w).Encode(post)
 		}
@@ -114,6 +163,8 @@ func returnSinglePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
+	refreshBlogposts()
+	fmt.Println("SHNOODIEDOODLE")
 	// http.HandleFunc("/", homePage)
 	// http.HandleFunc("/posts", returnAllPosts)
 	myRouter := mux.NewRouter().StrictSlash(true)
@@ -121,4 +172,5 @@ func handleRequests() {
 	myRouter.HandleFunc("/posts", returnAllPosts)
 	myRouter.HandleFunc("/posts/{title}", returnSinglePost)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	fmt.Println(Blogposts)
 }
